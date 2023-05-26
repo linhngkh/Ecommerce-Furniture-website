964ee8fb303e9dbefc996851d92dbf12bc4f8b84
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-26 18:12:32 +0300

    image data file set up

---
+++ frontend/data.js
@@ -0,0 +1,69 @@
+export const featureProducts = [
+  {
+    id: 1,
+    img: "./src/assets/chair2.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    id: 2,
+    img: "./src/assets/chair3.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    id: 3,
+    img: "./src/assets/chair1.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    id: 4,
+    img: "./src/assets/chair4.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+];
+
+export const newProducts = [
+  {
+    img: "./src/assets/chair5.png",
+    name: "Comfort Handy Craft",
+    price: "€65.00",
+    discount: "€42.00",
+  },
+  {
+    img: "./assets/chair3.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    img: "./assets/chair7.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    img: "./assets/chair8.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    img: "./assets/chair9.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+  {
+    img: "./assets/chair4.png",
+    name: "Cantilever chair",
+    code: "Code - Y523201",
+    price: "€42.00",
+  },
+];
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,7 +1,27 @@
-
+import { featureProducts, newProducts } from "../../data";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <div>
+        {featureProducts.map((product) => (
+          <div key={product.index}>
+            {" "}
+            <img src={product.img} alt="" />
+            <p>{product.name}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        {newProducts.map((product) => (
+          <div key={product.index}>
+            {" "}
+            <img src={product.img} alt="" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Product;
