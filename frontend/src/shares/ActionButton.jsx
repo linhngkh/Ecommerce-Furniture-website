c1de7b954bdb855f4e518ab8cf90f2aa65567fc8
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-26 17:46:02 +0300

    product page building...

--- frontend/src/shares/ActionButton.jsx
+++ frontend/src/shares/ActionButton.jsx
@@ -1,6 +1,6 @@
 const ActionButton = ({ children }) => {
   return (
-    <button className="bg-pink px-8 py-2 text-sm text-white hover:bg-deeppink">
+    <button className=" mt-5 bg-pink px-8 py-2 text-sm text-white hover:bg-deeppink">
       {children}
     </button>
   );
