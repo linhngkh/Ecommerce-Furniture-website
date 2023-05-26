bbedbd2a0bef2324a5dbb3eb61a2bea2d25e16f5
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-26 17:06:16 +0300

    style hero section done

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -3,33 +3,37 @@ import Sofa from "../assets/sofa.png";
 import Lamp from "../assets/lamp.png";
 const HeroSection = () => {
   return (
-    <section className=" min-h-full w-full bg-primary">
-      <img src={Lamp} alt="lamp" />
-
-      <div className="flex  items-center px-10 py-20">
-        {/* text */}
-        <div className="flex-1 flex-col space-y-5">
-          <p className="text-sm text-pink">
-            Best Furniture For Your Castle....
-          </p>
-          <h1 className="font-jos text-3xl font-bold leading-10">
-            New Furniture Collection <br />
-            Trends in 2020
-          </h1>
-          <p className="text-sm text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
-            est adipiscing in phasellus non in justo.
-          </p>
-          <ActionButton>Shop Now</ActionButton>
+    <section className="bg-primary">
+      <div className=" flex min-h-full w-full ">
+        <div className="w-[20%]">
+          {" "}
+          <img src={Lamp} alt="lamp" />
         </div>
-        {/* images */}
-        <div className="flex-1">
-          <div className="relative ">
-            <img src={Sofa} alt="sofa" />
+
+        <div className="flex basis-3/4 items-center py-20 ">
+          {/* text */}
+          <div className="flex-1 flex-col space-y-5">
+            <p className="text-sm text-pink">
+              Best Furniture For Your Castle....
+            </p>
+            <h1 className="font-jos text-3xl font-bold leading-10">
+              New Furniture Collection <br />
+              Trends in 2020
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
+              est adipiscing in phasellus non in justo.
+            </p>
+            <ActionButton>Shop Now</ActionButton>
+          </div>
+          {/* images */}
+          <div className="flex-1">
+            <div className="relative ">
+              <img src={Sofa} alt="sofa" />
+            </div>
           </div>
         </div>
       </div>
-
       <div>dot dot dot </div>
     </section>
   );
