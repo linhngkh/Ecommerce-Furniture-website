67b554a2d6e64c85c21d3d5d836fad2462797bca
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-21 20:20:24 +0300

    update Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
-import Layout from "./shares/Layout";
-
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return <Layout></Layout>;
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+      }
+      if (window.screenY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div>
+      <Header isTopOfPage={isTopOfPage} />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -39,10 +39,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <Suspense fallback="loading">
-        <App />
-      </Suspense>
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
