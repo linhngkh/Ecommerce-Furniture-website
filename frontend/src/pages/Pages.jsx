a9853ef7fc6bd083a51381980f5de361bbbe96f7
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-23 21:21:23 +0300

    update grid card for mobile

--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -8,24 +8,25 @@ const Pages = () => {
       <div className="mx-auto flex h-[200px] items-center bg-primary px-20 md:px-20">
         <h1 className="font-jose text-2xl font-extrabold">Shop Grid Default</h1>
       </div>
-      {/* bars */}
-      <div className="flex h-full w-full items-center justify-center bg-white px-20 md:px-20">
+
+      <div className="flex h-full w-full items-center justify-center bg-white px-20 md:px-20 ">
         <div className="my-20">
-          <div className="flex justify-around ">
+          {/* bars */}
+          <div className="flex flex-col justify-around md:flex md:flex-row">
             <div className="">
               <h1 className="text-md font-jose font-semibold text-blue">
                 Ecommerce Acceories & Fashion item
               </h1>
             </div>
-            <div className="subtext-thin flex items-end justify-end gap-5">
-              <div className="">
+            <div className="subtext-thin flex flex-wrap gap-y-3 md:flex md:flex-row md:items-end md:justify-end md:gap-5">
+              <div className="flex  gap-3">
                 Per Page:
                 <input className="w-20 border-2" />
               </div>
-              <div>
+              <div className="flex gap-3">
                 Sort By: <select className="w-20 border-2"></select>
               </div>
-              <div className="flex  items-center justify-center gap-2">
+              <div className="flex items-center justify-center gap-1 md:gap-2">
                 View:
                 <BsGridFill />
                 <MdOutlineFormatListBulleted />
@@ -37,16 +38,16 @@ const Pages = () => {
           {/* product grid */}
           <div className="grid-column-4 mt-20">
             {pageProducts.map((item) => (
-              <div key={item.id} className="h-[340px] w-[230px] ">
+              <div key={item.id} className="h-[340px] w-[240px] ">
                 <div className="flex items-center justify-center bg-secondary p-4 hover:bg-[#EBF4F3]">
                   <img
                     src={item.img}
                     alt={item.name}
-                    className="w-full object-contain"
+                    className="h-48 w-full object-contain"
                     loading="lazy"
                   />
                 </div>
-                <div className="h-[100px] py-5">
+                <div className="flex-1  py-5">
                   <div className="text-center">
                     <p className="text-md font-jose font-extrabold text-[#151875]">
                       {item.name}
