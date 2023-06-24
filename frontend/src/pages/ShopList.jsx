57d993b51af5510b557cb303d808fd88a8ee2a75
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-24 10:46:17 +0300

    style done shop list page

--- frontend/src/pages/ShopList.jsx
+++ frontend/src/pages/ShopList.jsx
@@ -6,6 +6,7 @@ import { MdOutlineFormatListBulleted } from "react-icons/md";
 import { BsCart } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { SlMagnifierAdd } from "react-icons/sl";
+import { AiFillStar } from "react-icons/ai";
 
 import { listScenes } from "../../data";
 
@@ -14,13 +15,13 @@ const ShopList = () => {
     <section className="md:pt-20">
       {/* banner title */}
       <SharedBanner>Shop List</SharedBanner>
-      <div className="flex h-full w-full items-center justify-center bg-white px-20 md:px-20 ">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-white px-20 md:flex-col md:px-20 ">
         {/* bars */}
         <div className="my-20">
           <div className="flex flex-col justify-between md:flex md:flex-row">
             <SharedTitle />
             <div className="subtext-thin flex flex-wrap gap-y-3 md:flex md:flex-row md:items-end md:justify-end md:gap-5">
-              <div className="flex  gap-3">
+              <div className="flex gap-3">
                 Per Page:
                 <input className="w-20 border-2" />
               </div>
@@ -38,28 +39,56 @@ const ShopList = () => {
         </div>
 
         {/* products */}
-        <div className="mt-20">
+        <div className="">
           {listScenes.map((scene) => (
-            <div key={scene.title}>
+            <div
+              key={scene.title}
+              className="flex flex-col items-center justify-center gap-7 p-6 md:flex-row"
+            >
               <div>
-                <img src={scene.image} alt={scene.title} />
+                <img
+                  src={scene.image}
+                  alt={scene.title}
+                  className="w-[300px] object-contain md:w-full"
+                  loading="lazy"
+                />
               </div>
-              <div>
-                <p>{scene.title}</p>
-                <div>
-                  <p>{scene.discount}</p>
-                  <p>{scene.price}</p>
-                  <p></p> {/* star */}
-                  <p>{scene.desc}</p>
-                  <span>
-                    <BsCart />
-                  </span>
-                  <span>
-                    <AiOutlineHeart />
-                  </span>
-                  <span>
-                    <SlMagnifierAdd />
-                  </span>
+
+              <div className="flex flex-col justify-center leading-8">
+                <h3 className="text-md font-jose font-extrabold text-[#111C85]">
+                  {scene.title}
+                </h3>
+                <div className="md:w-[650px]">
+                  <div className="flex gap-3 font-jose">
+                    <p className="text-sm font-thin text-[#111C85]">
+                      {scene.discount}
+                    </p>
+                    <p className="text-sm font-thin text-pink line-through ">
+                      {scene.price}
+                    </p>
+                    <div className="flex text-orange-300">
+                      <AiFillStar />
+                      <AiFillStar />
+                      <AiFillStar />
+                      <AiFillStar />
+                      <AiFillStar />
+                    </div>
+                  </div>
+
+                  <p className="font-sm whitespace-normal text-subtext">
+                    {scene.desc}
+                  </p>
+                  <div className="mt-2 flex justify-center gap-8 text-[#111C85] md:justify-start">
+                    <span>
+                      <BsCart />
+                    </span>
+                    <span>
+                      <AiOutlineHeart />
+                    </span>
+                    <span>
+                      <SlMagnifierAdd />
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
