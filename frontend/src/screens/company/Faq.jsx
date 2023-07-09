922dfcbebde9d0c04e9666bd5f25b1c7e3c05a99
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-09 20:57:49 +0300

    update style for faq page

--- frontend/src/screens/company/Faq.jsx
+++ frontend/src/screens/company/Faq.jsx
@@ -1,12 +1,11 @@
 import ActionButton from "../../shares/ActionButton";
 import SharedBanner from "../../shares/SharedBanner";
-import SharedText from "../../shares/SharedText";
 
 const Faq = () => {
   return (
     <section className="pt-10 md:pt-20 ">
       <SharedBanner>FAQ</SharedBanner>
-      <div className="flex  px-10  py-10 md:px-20">
+      <div className="flex  flex-col px-10  py-20 md:flex-row md:px-20">
         <div className=" w-[50%] space-y-10 pb-3">
           <h1 className="font-jose text-3xl font-bold text-blue">
             General Information
@@ -20,15 +19,42 @@ const Faq = () => {
               sed tristique mollis vitae, consequat gravida sagittis.
             </p>
           </div>
+          <div className="w-[600px] space-y-4 leading-loose">
+            <p className="font-jose text-sm font-bold text-blue">
+              Magna bibendum est fermentum eros.
+            </p>
+            <p className="font-jose text-sm text-subtext">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
+              sed tristique mollis vitae, consequat gravida sagittis.
+            </p>
+          </div>
+          <div className="w-[600px] space-y-4 leading-loose">
+            <p className="font-jose text-sm font-bold text-blue">
+              Odio muskana hak eris conseekin sceleton?
+            </p>
+            <p className="font-jose text-sm text-subtext">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
+              sed tristique mollis vitae, consequat gravida sagittis.
+            </p>
+          </div>
+          <div className="w-[600px] space-y-4 leading-loose">
+            <p className="font-jose text-sm font-bold text-blue">
+              Elit id blandit sabara boi velit gua mara?
+            </p>
+            <p className="font-jose text-sm text-subtext">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
+              sed tristique mollis vitae, consequat gravida sagittis.
+            </p>
+          </div>
         </div>
         {/* form */}
-        <div className="flex flex-col bg-secondary ">
+        <div className="flex flex-col bg-secondary md:flex-col">
           <h3 className="mt-10 px-10 font-jose text-xl font-bold">
             Ask a Question
           </h3>
           <form
             action=""
-            className="flex h-[738px] w-[566px] flex-col space-y-6 px-10 py-10"
+            className="flex flex-col space-y-6 px-10 py-10 md:h-[538px] md:w-[566px] "
           >
             <input
               type="text"
