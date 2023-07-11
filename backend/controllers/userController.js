ab8c78f3c343e4c9868a6119d2728d77537235b6
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-11 21:31:45 +0300

    user routes for CRUD

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,4 +7,37 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
 });
 
-export { authUser };
+// Register a new user
+//route POST /api/users
+//access public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "register user" });
+});
+
+// Log put user
+//route POST /api/users/logout
+//access public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logput user" });
+});
+
+// Get user profile
+//route GET /api/users/profile
+//access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "profile user" });
+});
+
+// Update user profile
+//route PUT /api/users/profile
+//access private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "update profile user" });
+});
+export {
+  authUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  registerUser,
+};
