c63526cd13ebe8fd4363cc5374c19eb9e904dc51
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-12 17:08:30 +0300

    auth route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import generateToken from "../utils/generateToken.js";
 //route POST /api/users/auth
 //access public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // Register a new user
@@ -43,9 +57,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // Log put user
 //route POST /api/users/logout
 //access public
-const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "logput user" });
-});
+const logoutUser = asyncHandler(async (req, res) => {});
 
 // Get user profile
 //route GET /api/users/profile
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,10 @@ userSchema.pre("save", async function (next) {
   this.password = await bcript.hash(this.password, salt);
 });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcript.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
