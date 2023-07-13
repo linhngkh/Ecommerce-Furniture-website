12fedf303d1f65ccbc36d64b30b5d041c58a938d
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-13 13:27:06 +0300

    update backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ const app = express();
 // middleware
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://frontend-furniture.firebaseapp.com",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
