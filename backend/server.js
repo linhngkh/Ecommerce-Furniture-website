9f9ba6efbed9cff1efb00c943502c54e9fb3f23c
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-13 14:01:20 +0300

    update server

--- backend/server.js
+++ backend/server.js
@@ -3,43 +3,27 @@ import dotenv from "dotenv";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
+const app = express();
 import colors from "colors";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
 
 //connect mongoDB
 import connectDB from "./config/db.js";
 connectDB();
 
-import userRoutes from "./routes/userRoutes.js";
-
-const app = express();
-
 // middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: "Content-Type, Authorization",
-  })
-);
+app.use(cors());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// // Define the CORS middleware
-// const corsMiddleware = (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // Update with your allowed domain
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "POST, PATCH, OPTIONS");
-//   next();
-// };
-
-// // Apply the CORS middleware to all routes
-// app.use(corsMiddleware);
 
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
