85a2b0865166e9c4f80fe8f9d1eb36a3f47c5c43
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-14 17:04:13 +0300

    update optimize

--- frontend/src/pages/page-grid/Sorting.jsx
+++ frontend/src/pages/page-grid/Sorting.jsx
@@ -1,22 +1,18 @@
-const Sorting = ({ onChangeHandler, sortBy, sorting }) => {
+const Sorting = ({ sortOptions, handleSortChange, options }) => {
   return (
     <div className="flex gap-3">
-      <form action="">
-        {" "}
-        <label htmlFor="sortby">Sort By:</label>
-        <select
-          value={sortBy}
-          className="w-20 border-2"
-          name="products"
-          id="products"
-          onClick={sorting}
-        >
-          <option value="price" onChange={onChangeHandler}>
-            Sort by Price
+      <label value="">Sort By</label>
+      <select
+        value={options}
+        onChange={handleSortChange}
+        className="w-20 border-2"
+      >
+        {sortOptions.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
           </option>
-          <option value="name">Sort by Name</option>
-        </select>
-      </form>
+        ))}
+      </select>
     </div>
   );
 };
