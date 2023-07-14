cbc809e4ce006ede4ac9d4d8560f72da0e3fb7b2
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-14 08:51:29 +0300

    update backend api data

--- backend/products.js
+++ backend/products.js
@@ -1,25 +1,74 @@
-const products = [
+export const products = [
   {
-    id: 1,
-    name: "Chair",
-    desc: "Chair",
-    price: 67,
-    image: "",
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/afdk0p6rwhyk52tg0wbe.png",
+    name: "Vel elit euismod",
+    price: "€26.00",
+    discount: "€49.00",
   },
   {
-    id: 2,
-    name: "Chair",
-    desc: "Chair",
-    price: 67,
-    image: "",
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/zgazuaazpcpqrlbv6a06.png",
+    name: "Jonathan adler chair",
+    price: "€26.00",
+    discount: "€49.00",
   },
   {
-    id: 3,
-    name: "Chair",
-    desc: "Chair",
-    price: 67,
-    image: "",
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/n2kxltaonm3tuc6zefvl.png",
+    name: "Jonathan adler chair",
+    price: "€26.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/s0gnso7r5utqehl8wwhx.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/ongpxkjquszpuizypfjd.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/g56z5dnvhofeiav2rvu2.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/wgalia8uym0rkeai6p6n.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/udah2uclwuo8uirv79az.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/inh7s2hrfl6kcjrnsylf.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/fqb8s4yiftsis77b4jna.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/miw3utwmlffmury67jaj.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
+  },
+  {
+    img: "https://res.cloudinary.com/linh123/image/upload/v1689313512/s0gnso7r5utqehl8wwhx.png",
+    name: "Sed at fermentum",
+    price: "€32.00",
+    discount: "€49.00",
   },
 ];
-
-export { products };
