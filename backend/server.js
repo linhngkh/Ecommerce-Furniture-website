98a56a36b16e75b6be57fa6515c86812cef08607
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-23 14:56:51 +0300

    bodyparser

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 import colors from "colors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import cors from "cors";
 // import { corsOptions } from "./config/corsOptions.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -19,6 +20,7 @@ connectDB();
 // middleware
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
