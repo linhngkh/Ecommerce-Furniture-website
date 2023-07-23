88c9771ab292ba34b0435f46d0125a31001bff6a
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-23 14:58:14 +0300

    update link

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -4,6 +4,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://backend-stores.vercel.app",
   prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
     headers.set("Access-Control-Allow-Origin", "*");
     return headers;
   },
